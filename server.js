2f816f275b0840ff25d9c2d082a24dcb250d1b74
Josh Boustead <josh.boustead@pemdastech.com>
2023-05-01 22:15:57 -0500

    Update the start script in the package.json.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
